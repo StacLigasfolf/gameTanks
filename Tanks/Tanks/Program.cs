7de4958756aff71d3fdf99d377f1ef20c80ec12f
Ashot Petrosyan <65871508+StacLigasfolf@users.noreply.github.com>
2022-12-06 13:29:28 +0300

    Разделил функции стрельбы на несколько частей, отделил вывод информации техники в отдельную функцию

--- Tanks/Tanks/Program.cs
+++ Tanks/Tanks/Program.cs
@@ -10,7 +10,6 @@ namespace Game
         public int bullet;
         public double domage;
 
-
         public Items(int arm, double heal, int bull, double dom)
         {
             armor = arm;
@@ -18,8 +17,13 @@ namespace Game
             bullet = bull;
             domage = dom;
         }
+        enum UnitStep 
+        { 
+            human,
+            robot  
+        }
 
-        public void PrintInfo(Items personal, Items enemy)
+        public void PrintInfo(Items personal)
         {
             Console.WriteLine($"ЗДОРОВЬЕ: {personal.health}");
             Console.WriteLine("/////////////////////////////////");
@@ -28,39 +32,44 @@ namespace Game
             Console.WriteLine($"ПАТРОНЫ: {personal.bullet}");
         }
 
+        public void DamageOrginazer(Items personal, Items enemy)
+        {
+            personal.bullet -= 1;
+            enemy.health = enemy.health - personal.domage + personal.armor;
+            personal.domage += personal.domage * 0.2;
+
+            if (enemy.armor < 7)
+            {
+                enemy.armor += 1;
+            }
+
+            Console.WriteLine($"КРИТИЧЕСКИЙ УРОН: + {personal.domage}");
+        }
+
         /* Функция регистрации урона и изменения ЗДОРОВЬЯ, ПАТРОНОВ, УРОНА
          Дублирующиеся (Console.WriteLine) чисто для красоты и удобства пользователя*/
         public void Shooting(Items personal, Items enemy, Random random)
         {
             Console.WriteLine("ВЫСТРЕЛ!!!");
             // Диапазон вероятности промаха 
-            int probability = random.Next(0, 101);
 
-            if (probability >= 80)
+
+            if (random.Next(0, 101) >= 80)
             {
                 personal.bullet -= 1;
                 Console.WriteLine("ПРОМАХ!!!");
-                PrintInfo(personal, enemy);
+                PrintInfo(personal);
             }
             // Условие вероятности критического урона 
-            else if (probability >= 90)
+            else if (random.Next(0, 101) >= 90)
             {
-                personal.bullet -= 1;
-                enemy.health = enemy.health - personal.domage + personal.armor;
-                personal.domage += personal.domage * 0.2;
-
-                if (enemy.armor < 7)
-                {
-                    enemy.armor += 1;
-                }
-
-                Console.WriteLine($"КРИТИЧЕСКИЙ УРОН: + {personal.domage}");
-                PrintInfo(personal, enemy);
+                DamageOrginazer(personal, enemy);
+                PrintInfo(personal);
             }
-            else if (personal.bullet == 0)
+            else if (personal.bullet - 1 < 0)
             {
                 Console.WriteLine("У ВАС НЕТ БОЕПРИПАСОВ!!!");
-                PrintInfo(personal, enemy);
+                PrintInfo(personal);
             }
             else
             {
@@ -71,51 +80,53 @@ namespace Game
                 {
                     enemy.armor += 1;
                 }
-                PrintInfo(personal, enemy);
+                PrintInfo(personal);
             }
 
         }
 
         /* Функция лечения принимающая случайное количество hp и прибовляет его к текущему
         Так-же установлен предел для значения health в 100 */
-        public void Mending(Items personal,Items enemy, int hp)
+        public void Mending(Items personal, int hp)
         {
-            if (personal.health + hp > 100)
+            if (personal.health + hp >= 100)
             {
                 personal.health = 100;
-                Console.WriteLine($"Вылечился до {personal.health} hp");
-                PrintInfo(personal, enemy);
+                Console.WriteLine($"У ВАС ПОЛНОЕ ЗДОРОВЬЕ!!!");
+                PrintInfo(personal);
             }
             else
             {
-                personal.health = personal.health + hp;
-                Console.WriteLine($"Вылечился до {personal.health} hp");
-                PrintInfo(personal, enemy);
+                personal.health += hp;
+                Console.WriteLine($"+{hp} hp");
+                PrintInfo(personal);
             }
         }
 
         /* Функция покупки патронов, прибовляет у текущему значению случайное в определенном диапазоне*/
-        public void ByBullet(int addBullet, Items personal, Items enemy)
+        public void ByBullet(int addBullet, Items personal)
         {
             personal.bullet += addBullet;
-            Console.WriteLine($"куплено {addBullet} боеприпасов");
-            PrintInfo(personal, enemy);
+            Console.WriteLine($"+{addBullet} ПАТРОНОВ");
+            PrintInfo(personal);
         }
 
         // Функция хода
         public void Steps(Items personal, Items enemy, int step, Random random)
         {
-            if(step == 1)
+            switch (step)
             {
-                personal.Shooting(personal, enemy, random);
-            }else if(step == 2) 
-            {
-                int hp = random.Next(5, 10);
-                personal.Mending(personal, enemy, hp);
-            }else if(step == 3)
-            {
-                int bullets = random.Next(1, 4);
-                personal.ByBullet(bullets, personal, enemy);
+                case 1:
+                    personal.Shooting(personal, enemy, random);
+                    break;
+                case 2:
+                    int hp = random.Next(5, 10);
+                    personal.Mending(personal, hp);
+                    break;
+                case 3:
+                    int bullets = random.Next(1, 4);
+                    personal.ByBullet(bullets, personal);
+                    break;
             }
         }
 
@@ -125,9 +136,11 @@ namespace Game
             {
                 Items tank = new(4, 100, 10, 20);
                 Items robot = new(4, 100, 10, 20);
+
+                UnitStep unit = new();
                 Random random = new();
 
-                bool move = true;
+                
                 // Основной цикл игры 
                 while (true)
                 {
@@ -145,23 +158,24 @@ namespace Game
                     // Проверка корректности введенных пользователем данных
                     try
                     {
-                        switch (move)
+                        switch (unit)
                         {   
-                            case true:
+                            case UnitStep.human:
 
                                 Console.WriteLine("***************************** ТАНК *****************************");
                                 Console.WriteLine("1. Огонь \n2. Ремонт\n3. Купить патроны");
                                 int step = Convert.ToInt32(Console.ReadLine());
                                 tank.Steps(tank, robot, step, random);
-                                move = !move;
-                                break;
-
-                            case false:
+                                goto case UnitStep.robot;                    
+                                
+                            case UnitStep.robot:
 
                                 Console.WriteLine("***************************** РОБОТ *****************************");
                                 int steps = random.Next(1, 4);
                                 robot.Steps(robot, tank, steps, random);
-                                move = true;
+                                break;
+
+                            default:
                                 break;
                         }
                     }
